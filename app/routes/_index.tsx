905a11cf572d5e44871c16b04e555583dec315b4
Manuel Alanis <manuelaag99@gmail.com>
2023-07-11 10:52:42 -0600

    add prisma

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,3 @@
-// import type { LinksFunction } from "@remix-run/node";
-// import stylesUrl from "../styles/index.css";
-
-// export const links: LinksFunction = () => [
-//     { rel: "stylesheet", href: stylesUrl },
-// ];
-
 export default function RoutesIndex () {
     return (
         <div>
--- app/routes/frustrations.new.tsx
+++ app/routes/frustrations.new.tsx
@@ -1,12 +1,13 @@
 export default function AddFrustration () {
     return (
-        <div>
-            <form method="post" action="">
-                <label htmlFor="new-frustration">Share YOUR frustration!
-                    <input id="new-frustration" type="text" />
-                </label>
-                <button type="submit">SHARE</button>
-            </form>
-        </div>
+        <form method="post" action="">    
+            <label className="forms forms-label" htmlFor="new-frustration">Share YOUR frustration!
+                <input className="forms forms-input" id="new-frustration" type="text" required />
+            </label>
+            <label className="forms forms-label" htmlFor="new-frustration-author">Author (optional)
+                <input className="forms forms-input" id="new-frustration-author" type="text" />
+            </label>
+            <button className="forms forms-button" type="submit">SHARE</button>
+        </form>
     );
 }
