71de415e235e7f89d4f0b8b54eb7a35bce2be351
Manuel Alanis <manuelaag99@gmail.com>
2023-07-11 22:27:19 -0600

    functioning add frustrations route

--- app/routes/frustrations._index.tsx
+++ app/routes/frustrations._index.tsx
@@ -1,16 +1,31 @@
 import specificFrustrationStylesUrl from "../styles/specific-frustration-styles.css";
-import type { LinksFunction } from "@remix-run/node";
+import { json, type LinksFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: specificFrustrationStylesUrl },
 ];
 
+export const loader = async () => {
+    const count = await db.frustration.count();
+    const randomRowNumber = Math.floor(Math.random() * count);
+    const [randomFrustration] = await db.frustration.findMany({
+        skip: randomRowNumber,
+        take: 1
+    });
+    return json({ randomFrustration });
+};
+
 export default function FrustrationsIndexRoute () {
+    const data = useLoaderData<typeof loader>();
+
     return (
         <div className="frustration-display">
             <h3>Here's someone's frustration:</h3>
-            <p className="frustration">"I feel useless all the time."</p>
-            <p className="author">Maurice</p>
+            <p className="frustration">"{data.randomFrustration.content}"</p>
+            <p className="author">{data.randomFrustration.author}</p>
         </div>
     );
 }
--- app/routes/frustrations.new.tsx
+++ app/routes/frustrations.new.tsx
@@ -1,3 +1,21 @@
+import { redirect, type ActionArgs } from "@remix-run/node";
+
+import { db } from "../utils/db.server";
+
+export const action = async ({ request }: ActionArgs) => {
+    const form = await request.formData();
+    const content = form.get("content");
+    const author = form.get("author");
+    if (typeof content !== "string" || typeof author !== "string") {
+        throw new Error("Form not submitted correctly!");
+    }
+
+    const fields = { content, author };
+
+    const frustration = await db.frustration.create({ data: fields });
+    return redirect(`/frustrations/${frustration.id}`)
+}
+
 export default function AddFrustration () {
     return (
         <form className="forms-container" method="post" action="">    
